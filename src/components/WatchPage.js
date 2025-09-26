0bfdf70c2fad89b5bd34f97b1e3c051bad25153c
wisdomnkuna <butlharib@gmail.com>
2025-09-26 23:13:33 +0200

    Update WatchPage.js

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -24,18 +24,6 @@ const WatchPage = () => {
         <div className="lg:pr-6 lg:lg:max-w-[820px] ">
           <div className="sticky top-14 sm:static">
             <div className="relative pb-[56%] h-0 md:pb-[calc(43%)] lg:static lg:pb-0 lg:h-full">
-              <iframe
-                className="absolute w-full h-full md:min-h-[340px] lg:min-h-[415px] lg:static "
-                src={
-                  "https://www.youtube.com/embed/" +
-                  searchParams.get("v") +
-                  "?autoplay=1"
-                }
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
             </div>
           </div>
           <WatchVideoDetails />
